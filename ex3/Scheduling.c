f09163dadd5f23e2477319a00a5c712d2c0c1336
Viktor Naas <viktorn@stud.ntnu.no>
2022-09-19 11:58:25 +0200

    TASK B COMPLETED

--- ex3/Scheduling.c
+++ ex3/Scheduling.c
@@ -46,7 +46,7 @@ void *task_handler(void *var) {
 		}
 		usleep(timeUnitUs);
 	}
-	printf("Task with ID %d finising after having worked for %d out of %d time units \n", task->ID, task->currentRuntime, task->totalRuntime);
+	printf("Task with ID %d finishing after having worked for %d out of %d time units \n", task->ID, task->currentRuntime, task->totalRuntime);
 	fflush(stdout);
 	set_task_state(task, finished);
 	return NULL;
@@ -103,7 +103,7 @@ void round_robin(struct Task ** tasks, int taskCount, int timeout, int interval)
 }
 
 // Implement your schedulers here!
-void first_come_first(struct Task ** tasks, int taskCount, int timeout, int interval){
+void first_come_first(struct Task ** tasks, int taskCount, int timeout){
 	printf("Started FCFS scheduler with %d tasks \n", taskCount);
 	fflush(stdout);
 	int totalTimePassed = 0;
@@ -116,54 +116,241 @@ void first_come_first(struct Task ** tasks, int taskCount, int timeout, int inte
 		printf("Scheduler running at time %d \n", totalTimePassed);
 		fflush(stdout);
 		
+		for(int i = 0; i < taskCount; i++){
+			if(tasks[i] ->startTime < currentTaskStartTime || currentTaskStartTime == -1){
+				if (tasks[i] -> state != finished){
+				currentTaskIndex = i;
+				currentTaskStartTime = tasks[i] -> startTime;
+				}
+			}
+		}
+		// printf("TASK INDEX: %i \n", currentTaskIndex);
+		if(tasksCompleted < taskCount){
+			set_task_state(tasks[currentTaskIndex], running);
+			wait_for_rescheduling(tasks[currentTaskIndex] -> totalRuntime+10, tasks[currentTaskIndex]);
+			set_task_state(tasks[currentTaskIndex], finished);
+			currentTaskStartTime = -1;
+			tasksCompleted++;
+			totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
+		}
+		if(tasksCompleted >= taskCount){
+			totalTimePassed++;
+		}
 		if(totalTimePassed >= timeout){
 			printf("Scheduler done \n");
+			if(tasksCompleted == taskCount){
+				printf("All tasks completed\n");
+			}
 			fflush(stdout);
 			return;
 		}
-		for(int i = 0; i < taskCount; i++){
-			if((tasks[i] ->startTime < currentTaskStartTime || currentTaskStartTime == -1) && tasks[i] -> state != finished){
-				currentTaskIndex = i;
-				currentTaskStartTime = tasks[i] -> startTime;
-			}
-		}
-		// counter = 0;
-		// while(counter < taskCount){
-		// 	currentTaskIndex = (currentTaskIndex + 1) % taskCount;
-		// 	if(tasks[currentTaskIndex]->state != finished && tasks[currentTaskIndex]->startTime <= totalTimePassed){
-		// 		break;
-		// 	}
-		// 	counter ++;
-		// }
-
-		// if(counter >= taskCount){
-		// 	totalTimePassed ++; //Speeding through areas without tasks to schedule
-		// } else {
-		// 	if(prevTaskIndex != currentTaskIndex && prevTaskIndex != -1 && tasks[prevTaskIndex]->state != finished){ // prevTaskIndex == -1 means that this is the first task to be selected
-		// 		set_task_state(tasks[prevTaskIndex], preempted);
-		// 	}
-		set_task_state(tasks[currentTaskIndex], running);
-		wait_for_rescheduling(tasks[currentTaskIndex] ->totalRuntime, tasks[currentTaskIndex]);
-		set_task_state(tasks[currentTaskIndex], finished);
-
-		totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
-
-		if(tasksCompleted >= taskCount || totalTimePassed >= timeout){
-
-		}
-		// }
 	}
 }
 
-void shortest_process_next(struct Task ** tasks, int taskCount, int timeout, int interval){
-
+void shortest_process_next(struct Task ** tasks, int taskCount, int timeout){
+	printf("Started SPN scheduler with %d tasks \n", taskCount);
+	fflush(stdout);
+	int totalTimePassed = 0;
+	// int currentTaskIndex = 0;
+	// int prevTaskIndex = -1;
+	int tasksCompleted = 0;
+	int currentTaskRunTime = -1;
+	int currentTaskIndex = 0;
+	while(1){
+		printf("Scheduler running at time %d \n", totalTimePassed);
+		fflush(stdout);
+		
+		for(int i = 0; i < taskCount; i++){
+			if(tasks[i] ->totalRuntime < currentTaskRunTime || currentTaskRunTime == -1){
+				if (tasks[i] -> state != finished){
+				currentTaskIndex = i;
+				currentTaskRunTime = tasks[i] -> totalRuntime;
+				}
+			}
+		}
+		// printf("TASK INDEX: %i \n", currentTaskIndex);
+		if(tasksCompleted < taskCount){
+			set_task_state(tasks[currentTaskIndex], running);
+			wait_for_rescheduling(tasks[currentTaskIndex] -> totalRuntime+10, tasks[currentTaskIndex]);
+			set_task_state(tasks[currentTaskIndex], finished);
+			currentTaskRunTime = -1;
+			tasksCompleted++;
+			totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
+		}
+		if(tasksCompleted >= taskCount){
+			totalTimePassed++;
+		}
+		if(totalTimePassed >= timeout){
+			printf("Scheduler done \n");
+			if(tasksCompleted == taskCount){
+				printf("All tasks completed\n");
+			}
+			fflush(stdout);
+			return;
+		}
+	}
 }
 
-void highest_Rr_next(struct Task ** tasks, int taskCount, int timeout, int interval){
-
+double calculate_response_ratio(struct Task* task, int totalTimePassed){
+	double s = (task->totalRuntime - task->currentRuntime);
+	double w = totalTimePassed - task -> startTime;
+	return (w + s)/s;
 }
 
+void highest_RR_next(struct Task ** tasks, int taskCount, int timeout){
+	printf("Started HRRN scheduler with %d tasks \n", taskCount);
+	fflush(stdout);
+	int totalTimePassed = 0;
+	// int currentTaskIndex = 0;
+	// int prevTaskIndex = -1;
+	int tasksCompleted = 0;
+	int currentResponseRatio = -100000;
+	int currentTaskIndex = 0;
+	while(1){
+		printf("Scheduler running at time %d \n", totalTimePassed);
+		fflush(stdout);
+		
+		if(tasksCompleted < taskCount){
+			for(int i = 0; i < taskCount; i++){
+				if (tasks[i] -> state != finished){
+					double taskResponseRatio = calculate_response_ratio(tasks[i], totalTimePassed);
+					printf("RR: %f for index:%d \n", taskResponseRatio, i);
+					if(taskResponseRatio > currentResponseRatio || currentResponseRatio == -100000){
+						currentTaskIndex = i;
+						currentResponseRatio = taskResponseRatio;
+						
+					}
+				}
+			}
+			set_task_state(tasks[currentTaskIndex], running);
+			wait_for_rescheduling(tasks[currentTaskIndex] -> totalRuntime+10, tasks[currentTaskIndex]);
+			set_task_state(tasks[currentTaskIndex], finished);
+			currentResponseRatio = -100000;
+			tasksCompleted++;
+			totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
+		}
+		if(tasksCompleted >= taskCount){
+			totalTimePassed++;
+		}
+		if(totalTimePassed >= timeout){
+			printf("Scheduler done \n");
+			if(tasksCompleted == taskCount){
+				printf("All tasks completed\n");
+			}
+			fflush(stdout);
+			return;
+		}
+	}
+}
 
+int next_start_time(struct Task** tasks, int taskCount){
+	int nextStartTime = -1;
+	for(int i = 0; i < taskCount; i++){
+		if(nextStartTime == -1 || tasks[i]->startTime < nextStartTime){
+			nextStartTime = tasks[i] -> startTime;
+		}
+	}
+}
+
+void shortest_remaining_time(struct Task ** tasks, int taskCount, int timeout){
+	printf("Started SRT scheduler with %d tasks \n", taskCount);
+	fflush(stdout);
+	int totalTimePassed = 0;
+	int tasksCompleted = 0;
+	int currentTaskRemainingTime = -1;
+	int currentTaskIndex = 0;
+	while(1){
+		printf("Scheduler running at time %d \n", totalTimePassed);
+		fflush(stdout);
+		for(int i = 0; i < taskCount; i++){
+			if (tasks[i]-> state != finished){
+				if(tasks[i] ->startTime >= totalTimePassed){
+					double rt = tasks[i]-> totalRuntime - tasks[i]-> currentRuntime;
+					if (rt < currentTaskRemainingTime){
+						currentTaskIndex = i;
+					}
+
+				}
+			}
+			// if(tasks[i] ->startTime < currentTaskRemainingTime || currentTaskRemainingTime == -1){
+			// 	if (tasks[i] -> state != finished){
+			// 	currentTaskIndex = i;
+			// 	currentTaskRemainingTime = tasks[i] -> totalRuntime;
+			// 	}
+			// }
+		}
+		// CALCULATE INTERVAL
+
+		// ADD INTERVAL TO TASK -> CURRENTRUNTIME
+		
+		// printf("TASK INDEX: %i \n", currentTaskIndex);
+		if(tasksCompleted < taskCount){
+			set_task_state(tasks[currentTaskIndex], running);
+			wait_for_rescheduling(tasks[currentTaskIndex] -> totalRuntime+10, tasks[currentTaskIndex]);
+			set_task_state(tasks[currentTaskIndex], finished);
+			currentTaskRemainingTime = -1;
+			tasksCompleted++;
+			totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
+		}
+		if(tasksCompleted >= taskCount){
+			totalTimePassed++;
+		}
+		if(totalTimePassed >= timeout){
+			printf("Scheduler done \n");
+			if(tasksCompleted == taskCount){
+				printf("All tasks completed\n");
+			}
+			fflush(stdout);
+			return;
+		}
+	}
+}
+
+void feedback(struct Task ** tasks, int taskCount, int timeout){
+	printf("Started FEEDBACK scheduler with %d tasks \n", taskCount);
+	fflush(stdout);
+	int totalTimePassed = 0;
+	// int currentTaskIndex = 0;
+	// int prevTaskIndex = -1;
+	int tasksCompleted = 0;
+	int currentResponseRatio = -100000;
+	int currentTaskIndex = 0;
+	while(1){
+		printf("Scheduler running at time %d \n", totalTimePassed);
+		fflush(stdout);
+		
+		if(tasksCompleted < taskCount){
+			for(int i = 0; i < taskCount; i++){
+				if (tasks[i] -> state != finished){
+					double taskResponseRatio = calculate_response_ratio(tasks[i], totalTimePassed);
+					printf("RR: %f for index:%d \n", taskResponseRatio, i);
+					if(taskResponseRatio > currentResponseRatio || currentResponseRatio == -100000){
+						currentTaskIndex = i;
+						currentResponseRatio = taskResponseRatio;
+						
+					}
+				}
+			}
+			set_task_state(tasks[currentTaskIndex], running);
+			wait_for_rescheduling(tasks[currentTaskIndex] -> totalRuntime+10, tasks[currentTaskIndex]);
+			set_task_state(tasks[currentTaskIndex], finished);
+			currentResponseRatio = -100000;
+			tasksCompleted++;
+			totalTimePassed += (tasks[currentTaskIndex]->currentRuntime);
+		}
+		if(tasksCompleted >= taskCount){
+			totalTimePassed++;
+		}
+		if(totalTimePassed >= timeout){
+			printf("Scheduler done \n");
+			if(tasksCompleted == taskCount){
+				printf("All tasks completed\n");
+			}
+			fflush(stdout);
+			return;
+		}
+	}
+}
 
 
 int main(){
@@ -188,7 +375,16 @@ int main(){
 	sleep(1); // Let everything stabilize
 	int schedulerTimeout = 250;
 	
-    round_robin(tasks, taskCount, schedulerTimeout, 10);
+    // round_robin(tasks, taskCount, schedulerTimeout, 10);
+    //TASK B:
+	//first_come_first(tasks, taskCount, schedulerTimeout);
+	//shortest_process_next(tasks, taskCount, schedulerTimeout);
+	//highest_RR_next(tasks, taskCount,schedulerTimeout);
+	
+	//TASK C:
+	shortest_remaining_time(tasks, taskCount,schedulerTimeout);
+	//feedback(tasks, taskCount,schedulerTimeout);
+
 
 	
 	for(int i = 0; i < taskCount; i++){
