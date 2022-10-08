500112b7ac91d19ded9f0470924e8c5dfc5e9152
Viktor Naas <viktorn@stud.ntnu.no>
2022-10-08 21:05:21 +0200

    Big Big loop

--- ex5/AVR32 Template/src/main.c
+++ ex5/AVR32 Template/src/main.c
@@ -8,17 +8,17 @@
 #define CONFIG_USART_IF (AVR32_USART2)
 
 // defines for BRTT interface
-#define TEST_A      AVR32_PIN_PA31
-#define RESPONSE_A  AVR32_PIN_PA30
-#define TEST_B      AVR32_PIN_PA29
-#define RESPONSE_B  AVR32_PIN_PA28
-#define TEST_C      AVR32_PIN_PA27
-#define RESPONSE_C  AVR32_PIN_PB00
-
+#define TEST_A AVR32_PIN_PA31
+#define RESPONSE_A AVR32_PIN_PA30
+#define TEST_B AVR32_PIN_PA29
+#define RESPONSE_B AVR32_PIN_PA28
+#define TEST_C AVR32_PIN_PA27
+#define RESPONSE_C AVR32_PIN_PB00
 
 __attribute__((__interrupt__)) static void interrupt_J3(void);
 
-void init(){
+void init()
+{
     sysclk_init();
     board_init();
     busy_delay_init(BOARD_OSC0_HZ);
@@ -30,27 +30,76 @@ void init(){
 
     stdio_usb_init(&CONFIG_USART_IF);
 
-    #if defined(__GNUC__) && defined(__AVR32__)
-        setbuf(stdout, NULL);
-        setbuf(stdin,  NULL);
-    #endif
+#if defined(__GNUC__) && defined(__AVR32__)
+    setbuf(stdout, NULL);
+    setbuf(stdin, NULL);
+#endif
 }
 
-__attribute__((__interrupt__)) static void interrupt_J3(void){
-
+__attribute__((__interrupt__)) static void interrupt_J3(void)
+{
+    int pinA = gpio_get_pin_interrupt_flag(TEST_A);
+    if (pinA)
+    {
+        // Probably more efficient to respond here
+        testAFlag = 1;
+        gpio_clear_pin_interrupt_flag(TEST_A);
+    }
+    int pinB = gpio_get_pin_interrupt_flag(TEST_B);
+    if (pinB)
+    {
+        testBFlag = 1;
+    }
+    int pinC = gpio_get_pin_interrupt_flag(TEST_C);
+    if (pinC)
+    {
+        testCFlag = 1;
+        gpio_clear_pin_interrupt_flag(TEST_C);
+    }
 }
 
-
-int main (void){
+volatile int testAFlag = 0;
+volatile int testAIntrFlag = 0;
+volatile int testBFlag = 0;
+volatile int testBIntrFlag = 0;
+volatile int testCFlag = 0;
+volatile int testCIntrFlag = 0;
+int main(void)
+{
     init();
 
-    while(1){
-        gpio_toggle_pin(LED0_GPIO);
+    gpio_enable_pin_interrupt(TEST_A, GPIO_BOTHEDGES); // GPIO_PIN_CHANGE?
+    gpio_enable_pin_interrupt(TEST_B, GPIO_BOTHEDGES);
+    gpio_enable_pin_interrupt(TEST_C, GPIO_BOTHEDGES);
 
-        printf("tick\n");
-
-        busy_delay_ms(500);
-        // testA();
-        // testAll();
+    while (1)
+    {
+        if (testAFlag)
+        {
+            gpio_set_pin_low(RESPONSE_A);
+            if (gpio_get_pin_value(TEST_A))
+            {
+                gpio_set_pin_high(RESPONSE_A);
+            }
+            testAFlag = 0;
+        }
+        if (testBFlag)
+        {
+            gpio_set_pin_low(RESPONSE_B);
+            if (gpio_get_pin_value(TEST_B))
+            {
+                gpio_set_pin_high(RESPONSE_B);
+            }
+            testBFlag = 0;
+        }
+        if (testCFlag)
+        {
+            gpio_set_pin_low(RESPONSE_C);
+            if (gpio_get_pin_value(TEST_C))
+            {
+                gpio_set_pin_high(RESPONSE_C);
+            }
+            testCFlag = 0;
+        }
     }
 }
